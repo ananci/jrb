3bf27a7fa92f4a54764f13e0098a1f41dc72f056
Anna Eilering <nahkki@gmail.com>
2016-04-24 16:57:25 -0500

    Began work on result objects.

--- jenkins_report_builder/main.py
+++ jenkins_report_builder/main.py
@@ -3,8 +3,17 @@ import jenkins
 
 import pprint
 
-class Result(object):
-    pass
+class ChartResults(object):
+
+    def __init__(object):
+        self.results = []
+
+class ChartResult(object):
+    def __init__(self, job_name, result, timestamp, trigger):
+        for key, value in locals().items():
+            if key != 'self':
+                setattr(self, key, value)
+
 
 class ViewReport(object):
 
